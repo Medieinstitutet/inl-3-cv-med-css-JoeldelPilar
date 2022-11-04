87c553370ec4691e196e9ea117bc03aba097d970
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2022-11-04 15:10:29 +0100

    Debugged js and replaced placholder text

--- js/main.js
+++ js/main.js
@@ -17,7 +17,25 @@ if ("sendEmail" !== undefined && "sendEmail" !== null) {}
 
 
 
-    function closeMenu() {
-        console.log("hej")
-        document.getElementById("hamburgerMenu").removeAttribute("checked");
-    }
+    // function closeMenu() {
+    //     console.log("hej")
+    //     document.getElementById("hamburgerMenu").removeAttribute("checked");
+    // }
+
+
+
+
+// <-----Credits Alex P FED22D ----->
+
+    //Lägg in en class till alla länkar i menyn, och skriv classen här
+const hreflink = document.querySelectorAll('.menu-link');
+// Lägg till Id till meny checkboxen och skriv Id här
+const myfield = document.getElementById('hamburgerMenu');
+//Foreach loops genom alla element som har classen du skrev ovanför
+hreflink.forEach(menulink => {
+    //Gör en "eventlistener" för alla dessa
+    menulink.addEventListener('click', function handleClick(event) {
+        //Om den ser att du trycker på dem, uncheckar checkboxen
+        myfield.checked = false;
+    });
+});
